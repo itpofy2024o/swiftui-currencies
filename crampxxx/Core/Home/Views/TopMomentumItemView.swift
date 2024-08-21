008a9cb126e0132c27fcbf8634b93cadf6ffcedd
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-21 21:25:03 +0800

    baby why

--- crampxxx/Core/Home/Views/TopMomentumItemView.swift
+++ crampxxx/Core/Home/Views/TopMomentumItemView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct TopMomentumItemView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "")
+        VStack(alignment: .leading) {
+            Image(systemName: "bitcoinsign.circle.fill")
                 .resizable()
                 .frame(width: 45,height: 45)
                 .foregroundColor(.green)
+                .padding(
+                    EdgeInsets(top: 0, leading: 5, bottom: 10, trailing: 0)
+                )
             HStack {
                 Text("HBAR")
                     .font(.caption)
@@ -21,11 +24,21 @@ struct TopMomentumItemView: View {
                 Text("20,0000.5678")
                     .font(.caption)
                     .foregroundColor(.secondary)
-            }
+            }.padding(
+                EdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 0)
+            )
             Text("+ 34%")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.blue)
+                .font(.title2)
+                .foregroundColor(.orange)
+                .padding(
+                    EdgeInsets(top: 0, leading: 3, bottom: 0, trailing: 0)
+                )
         }
+        .frame(width: 150, height: 150)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.systemGray2),lineWidth: 3.5)
+        )
     }
 }
 
